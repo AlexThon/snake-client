aa49cae9d8a014729ce2a430e464b8792da328d0
Alex Thon <ghaiquol@gmail.com>
2021-11-04 00:54:40 +0000

    add .write on connection

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+let name = "ANS ";
 const connect = function() {
   const conn = net.createConnection({
     host: 'localhost', // IP address here,
@@ -8,9 +9,13 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
     
-  
+  conn.write("BUL: HEy");
   conn.on('data', (data) => {
     console.log(data);
+  });
+ 
+  conn.on('connect', () => {
+    console.log(`Successfully connected...`);
   });
   
   return conn;
