6764adf00b1ec83fd6cea0288805dcb5872a8b77
Alex Thon <ghaiquol@gmail.com>
2021-11-04 22:07:37 +0000

    Add setupInput and handleUserInput

--- client.js
+++ client.js
@@ -1,24 +1,41 @@
 const net = require("net");
-let name = "ANS ";
+
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost', // IP address here,
+    host: "localhost" , // IP address here,
     port: 50541 // PORT number here,
   });
-  
+  //conn.write("Move: Up");
   // interpret incoming data as text
   conn.setEncoding("utf8");
     
-  conn.write("BUL: HEy");
-  conn.on('data', (data) => {
-    console.log(data);
-  });
- 
+  // conn.on('data', (data) => {
+  //   console.log(data);
+  // });
+  
   conn.on('connect', () => {
     console.log(`Successfully connected...`);
+    // conn.write("Name: BU");
   });
+
+  
+
   
   return conn;
 };
 
-module.exports = connect;
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // your code here
+  console.log("Sucessfully recorded...")
+};
+
+module.exports = {connect, setupInput, handleUserInput};
